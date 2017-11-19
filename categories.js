ffc23f331f6ddfb2ff2f3c7be3277aa5d4b4c5e3
Joshua <joshua.millien@gmail.com>
2017-11-19 15:37:36 +0100

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,4 @@
+
+ var obj = JSON.parse('{"categories" : [{"evenements":[{ "site1":"https://jsonformatter.org/"},{ "site2":"http://atinux.developpez.com/tutoriels/javascript/debuter-avec-socket-io/"}]  },{"actualites":"http://atinux.developpez.com/tutoriels/javascript/debuter-avec-socket-io/"},{"site3":"bifule"}]}');
+
+ console.log(obj.categories[0]);
