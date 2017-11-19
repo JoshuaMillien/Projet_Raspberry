5fcc4aeaa9b1715f5f034b2f433c610c54690f96
AmauryBeauchamp <beauchampamaury@hotmail.fr>
2017-11-19 16:30:10 +0100

    Add files via upload

---
+++ categoriesQuiMarche.js
@@ -0,0 +1,3 @@
+ var obj = JSON.parse('{"categories" : {"evenements":["https://jsonformatter.org/" , "http://atinux.developpez.com/tutoriels/javascript/debuter-avec-socket-io/"]}}');
+
+console.log(obj.categories.evenements[1]);
