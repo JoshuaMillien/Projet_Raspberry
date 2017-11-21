a69993d8665dadc38af2edee163c6d771479f608
Joshua <joshua.millien@gmail.com>
2017-11-21 16:12:41 +0100

    Delete categoriesQuiMarche.js

--- categoriesQuiMarche.js
+++
@@ -1,3 +0,0 @@
- var obj = JSON.parse('{"categories" : {"evenements":["https://jsonformatter.org/" , "http://atinux.developpez.com/tutoriels/javascript/debuter-avec-socket-io/"]}}');
-
-console.log(obj.categories.evenements[1]);
