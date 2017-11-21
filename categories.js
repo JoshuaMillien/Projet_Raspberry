5078456acaec588e44716bf9a8e21b9b5f3a0b1d
Joshua <joshua.millien@gmail.com>
2017-11-21 16:10:48 +0100

    Delete categories.js

--- categories.js
+++
@@ -1,4 +0,0 @@
-
- var obj = JSON.parse('{"categories" : [{"evenements":[{ "site1":"https://jsonformatter.org/"},{ "site2":"http://atinux.developpez.com/tutoriels/javascript/debuter-avec-socket-io/"}]  },{"actualites":"http://atinux.developpez.com/tutoriels/javascript/debuter-avec-socket-io/"},{"site3":"bifule"}]}');
-
- console.log(obj.categories[0]);
