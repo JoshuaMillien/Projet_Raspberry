40e84f6a98363b49951924a12a42900ceeb8c759
Joshua <joshua.millien@gmail.com>
2017-12-04 22:07:17 +0100

    Add files via upload
    Dans display.js, j'ai remplacé console.log par open.open qui permet de lancer l'url mais cela se fait sans cesse dans un nouvel onglet.
    Cela se lance dès qu'on se connecte en localhost:8080 sur le navigateur.

--- Display.js
+++ Display.js
@@ -1,9 +1,6 @@
-// Pour plus de lisibilité pour obj, aller voir fichier categories.json
-/*
+var open = require ('openurl');
 
-Ces 3 lignes permettent de lire un fichier json et de récupérer
-ses infos avec : jsonContent.categories.evenements[0] ...
-*/
+// open.open(url) permet de lancer ce qu'on recupère dans un nouvel onglet
 var fs = require("fs");
 var contents = fs.readFileSync('categories.json','utf8');
 var obj = JSON.parse(contents);
@@ -13,15 +10,17 @@ function timer(cptT,cptE,s){      //
         if ((cptE>s) && (cptT>s))
             cptE=cptT=0;          // si n dépasse le nombre de pages que l'on veut
                             // on le remet à 0 ==> retour au début du cycle
-        console.log(obj.categories.evenements[cptE++]);
+        open.open(obj.categories.evenements[cptE++]);
         setTimeout(function(){
-            console.log(obj.categories.theses[cptT++]);
+            open.open(obj.categories.theses[cptT++]);
         },3000); // Temps du setInterval / 2
     },6000);
 }
-
+exports.timer = timer;
+/*
 console.log(obj.categories.evenements[0]);
 setTimeout(function(){          // on commence le défilement afin d'éviter d'attendre au début
     console.log(obj.categories.theses[0]);
 },3000);                        // temps du setInterval / 2
-timer(1,1,2);                   // on commence à 1 étant donné qu'on commence à défiler ci-dessus
+//timer(1,1,2);                   // on commence à 1 étant donné qu'on commence à défiler ci-dessus
+*/
