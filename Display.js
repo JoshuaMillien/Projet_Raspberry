7482f5efd4d28087b8e93c701cfc573c710b80bd
Joshua <joshua.millien@gmail.com>
2017-12-05 21:39:55 +0100

    Add files via upload
    Mise en place d'un fichier de configuration. Display.js récupère les infos de ce fichier et éxécute tout comme il le faut.
    Le fichier de configuration fonctionne.

--- Display.js
+++ Display.js
@@ -1,26 +1,59 @@
 var open = require ('openurl');
 
+
 // open.open(url) permet de lancer ce qu'on recupère dans un nouvel onglet
 var fs = require("fs");
 var contents = fs.readFileSync('categories.json','utf8');
 var obj = JSON.parse(contents);
 
-function timer(cptT,cptE,s){      //
+var fileConf = fs.readFileSync('config.json','utf8');
+var config = JSON.parse(fileConf);
+
+function alterner(cptT,cptE,s){      //
     setInterval(function(){
         if ((cptE>s) && (cptT>s))
             cptE=cptT=0;          // si n dépasse le nombre de pages que l'on veut
                             // on le remet à 0 ==> retour au début du cycle
-        open.open(obj.categories.evenements[cptE++]);
+        console.log(obj.categories.evenements[cptE++]);
         setTimeout(function(){
-            open.open(obj.categories.theses[cptT++]);
-        },3000); // Temps du setInterval / 2
-    },6000);
+            console.log(obj.categories.theses[cptT++]); // pour ouvrir un onglet, remplacer les console.log par open.open
+        },config.vitesse/2); // Temps du setInterval / 2
+    },config.vitesse);
 }
-exports.timer = timer;
-/*
-console.log(obj.categories.evenements[0]);
-setTimeout(function(){          // on commence le défilement afin d'éviter d'attendre au début
-    console.log(obj.categories.theses[0]);
-},3000);                        // temps du setInterval / 2
-//timer(1,1,2);                   // on commence à 1 étant donné qu'on commence à défiler ci-dessus
-*/
+
+function alternerEvent(cptE,s){
+    setInterval(function(){
+        if (cptE>s)
+            cptE=0;
+        console.log(obj.categories.evenements[cptE++]);
+    },config.vitesse);
+}
+
+function alternerTheses(cptT,s){
+    setInterval(function(){
+        if (cptT>s)
+            cptT=0;
+        console.log(obj.categories.theses[cptT++]);
+    },config.vitesse);
+}
+
+
+if (config.categorie == "evenements")
+    alternerEvent(0,config.nombrePages-1);
+if (config.categorie == "theses")
+    alternerTheses(0,config.nombrePages-1);
+if (config.categorie == "evenements + theses"){
+    console.log(obj.categories.evenements[0]);
+    setTimeout(function(){          // on commence le défilement afin d'éviter d'attendre au début
+        console.log(obj.categories.theses[0]);
+    },config.vitesse/2);                        // temps du setInterval / 2
+    alterner(1,1,config.nombrePages-1);                   // on commence à 1 étant donné qu'on commence à défiler ci-dessus
+}
+
+
+
+
+
+
+
+exports.alterner = alterner;
--- testSock.js
+++ testSock.js
@@ -1,8 +1,6 @@
 var http = require('http');
 var fs = require('fs');
 var disp = require('./Display');
-var opn = require('openurl');
-
 // Chargement du fichier index.html affiché au client
 var server = http.createServer(function(req, res) {
     fs.readFile('./index.html', 'utf-8', function(error, content) {
@@ -16,7 +14,7 @@ var io = require('socket.io').listen(server);
 
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
-    disp.timer(1,1,2);
+    disp;   // lance les tests du fichier display.js
 });
 
 
